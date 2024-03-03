186300cc1a7bf40460c39a4193ffd0ea0bc58112
Your Name <you@example.com>
2024-03-03 11:30:29 -0600

    agregar clase contribucion

---
+++ contribucion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+class  contribucion{
+	private : sting nombres,apellidos,direccion;
+	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
