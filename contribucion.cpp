b510c62b60a2077edb2ac5207ae95bb9fe2dbc8e
Your Name <you@example.com>
2024-03-03 11:46:45 -0600

    agregar clase a AACP

--- contribucion.cpp
+++ contribucion.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
-class  contribucion{
-	private : sting nombres,apellidos,direccion;
-	
+class contribucion {
+	protected : string nombres,apellidos,direccion,fecha_nacimiento;
+				int telefono;
+	contribucion(){
+	}
+	contribucion(string nom, string ape, string dic, int tel, string fn){
+		nombres = nom;
+		apellidos = ape;
+		direccion = dic;
+		telefono = tel;
+		fecha_nacimiento = fn;
+	}
 	void crear();
 	void leer();
-	void actualizar();
+	void actualiza();
 	void borrar();
+	
+	
 };
