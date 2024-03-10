13d3bf74c364f11b9813940e6cc3e74ae002043f
Cesar-230405 <1cesarcastro2@gmail.com>
2024-03-10 08:00:04 -0600

    Agregar CACL

---
+++ CACL.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//César Ariel Castro Linares
+class CACL{
+      private : string nombres, apellidos, direccion;
+	  //CRUD
+	void Crear();
+	void Leer();
+	void Actualizar();
+	void Borrar();	
+};
