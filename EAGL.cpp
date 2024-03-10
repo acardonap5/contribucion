b9bcca7ddb8965e1d102b818ac7e1ba95e5677ee
edrei giron <egironl12@miumg.edu.gt>
2024-03-10 10:01:14 -0600

    Contribucion clase EAGL

---
+++ EAGL.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class EAGL{
+	private : string nombres,apellidos,direccion;
+	
+	void crear ();
+	void leer ();
+	void actualizar ();
+	void borar ();
+	
+};
